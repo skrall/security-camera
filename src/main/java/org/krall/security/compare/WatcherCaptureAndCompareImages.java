c3b2e062437207a6257c9fb83fb1c020787bc78a
Steve Krall <skrall@gmail.com>
2013-08-02 22:17:41 -0400

    Switch from Java WatcherService to inotifywait command line.
    Since the Java WatcherService doesn't notify on a close event, it was basically useless.  Use inotifywait instead, just have to handle output of command.

--- src/main/java/org/krall/security/compare/WatcherCaptureAndCompareImages.java
+++ src/main/java/org/krall/security/compare/WatcherCaptureAndCompareImages.java
@@ -3,18 +3,15 @@ package org.krall.security.compare;
 import javafx.concurrent.Task;
 import javafx.scene.image.Image;
 import org.krall.security.command.CaptureTimeLapseImage;
+import org.krall.security.command.INotifyWait;
 import org.krall.security.commandline.AppOptions;
 import org.krall.security.image.FXImageCompare;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardWatchEventKinds;
-import java.nio.file.WatchEvent;
-import java.nio.file.WatchKey;
-import java.nio.file.WatchService;
+import java.nio.file.Paths;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -23,78 +20,41 @@ public class WatcherCaptureAndCompareImages implements CaptureAndCompareImages {
 
     private static final Logger logger = LoggerFactory.getLogger(WatcherCaptureAndCompareImages.class);
 
+    private FXImageCompare imageCompare = new FXImageCompare();
+
+    private AppOptions options = AppOptions.getInstance();
+
+    private ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 1, 0, TimeUnit.SECONDS,
+                                                                 new ArrayBlockingQueue<Runnable>(1));
+
+    public WatcherCaptureAndCompareImages() {
+        imageCompare
+                .setParameters(options.getVerticalRegions(), options.getHorizontalRegions(), options.getSensitivity(),
+                               options.getStabilizer());
+        imageCompare.setDebugMode(2);
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy());
+    }
+
     @Override
     public void captureAndCompareImages() {
         logger.info("Starting to run capture...");
         CaptureTimeLapseImage captureTimeLapseImage = new CaptureTimeLapseImage();
         captureTimeLapseImage.run();
-        watchDirectory();
+        INotifyWait iNotifyWait = new INotifyWait(this);
+        iNotifyWait.run();
+        //watchDirectory();
         logger.info("Finished running capture.");
     }
 
-    private void watchDirectory() {
+    public void processFile(String file) {
+        Path image = null;
         try {
-            FXImageCompare imageCompare = new FXImageCompare();
-            AppOptions options = AppOptions.getInstance();
-            imageCompare.setParameters(options.getVerticalRegions(), options.getHorizontalRegions(),
-                                       options.getSensitivity(), options.getStabilizer());
-            imageCompare.setDebugMode(2);
-            ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 1, 0, TimeUnit.SECONDS,
-                                                                 new ArrayBlockingQueue<Runnable>(1));
-            executor.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy());
-            WatchService watcher = FileSystems.getDefault().newWatchService();
-            Path dir = AppOptions.getInstance().getImageDirectory().toPath();
-            dir.register(watcher, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY);
-            for (; ; ) {
-
-                // wait for key to be signaled
-                WatchKey key;
-                try {
-                    key = watcher.take();
-                } catch (InterruptedException x) {
-                    return;
-                }
-
-                for (WatchEvent<?> event : key.pollEvents()) {
-                    WatchEvent.Kind<?> kind = event.kind();
-
-                    // This key is registered only
-                    // for ENTRY_CREATE events,
-                    // but an OVERFLOW event can
-                    // occur regardless if events
-                    // are lost or discarded.
-                    if (kind == StandardWatchEventKinds.OVERFLOW) {
-                        continue;
-                    }
-
-                    // The filename is the
-                    // context of the event.
-                    WatchEvent<Path> ev = (WatchEvent<Path>) event;
-                    Path filename = ev.context();
-                    Path image = dir.resolve(filename);
-                    logger.info("Got event {} for file {}", kind, image);
-                    if (kind == StandardWatchEventKinds.ENTRY_CREATE) {
-                        try {
-                            executor.submit(new ImageProcessor(image, imageCompare));
-                        } catch (Exception ree) {
-                            logger.error(
-                                    "The raspistill is capturing images too fast.  Need to slow down the speed of " +
-                                    "capturing the images by increasing the -m flag.");
-                            Files.delete(image);
-                        }
-                    }
-                }
-
-                // Reset the key -- this step is critical if you want to
-                // receive further watch events.  If the key is no longer valid,
-                // the directory is inaccessible so exit the loop.
-                boolean valid = key.reset();
-                if (!valid) {
-                    break;
-                }
-            }
-        } catch (Exception e1) {
-            logger.error("Error while watching directory..", e1);
+            image = Paths.get(file);
+            executor.submit(new ImageProcessor(image, imageCompare));
+        } catch (Exception ree) {
+            logger.warn("The raspistill is capturing images too fast.  Need to slow down the speed of " +
+                        "capturing the images by increasing the -m flag.");
+            try { Files.delete(image); } catch (Exception ignore) {}
         }
     }
 
